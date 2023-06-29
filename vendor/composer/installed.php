b6801a204279f2b0c21d47c2f31102f8c725ffbc
Boone B Gorges <boonebgorges@gmail.com>
2023-06-29 15:01:24 -0500

    Track build files on build branch.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'openlab-at-city-tech/openlab-connections',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e892a69b38a88239d2a91d57af01f4245d69ccb4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'openlab-at-city-tech/openlab-connections' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e892a69b38a88239d2a91d57af01f4245d69ccb4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
